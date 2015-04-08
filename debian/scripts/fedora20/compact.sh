58105cac9ee2041205c5a8516b3404f7500a9599
Touqeer Hussain <touqeer.hussain@hotmail.co.uk@github.com>
2015-04-08 11:28:03 +0100

    deleting debian folder

--- debian/scripts/fedora20/compact.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-yum -y remove binutils glibc-headers glibc-devel libmpc mpfr kernel-devel kernel-headers cpp gcc perl* 
-yum clean all
-
-dd if=/dev/zero of=/junk bs=1M
-rm -f /junk
-
-sync
